2cd8faf464b676204591b13b44f97cdb3d0f23d6
ismaaa1x <ismael.rojas.herrera@est.una.ac.cr>
2025-08-26 22:32:08 -0600

    metodos de recetas en la clase lista, y la clase gestorDatosRecetas

---
+++ src/main/java/Despacho/Data/Listas/GestorDatosRecetas.java
@@ -0,0 +1,43 @@
+package Despacho.Data.Listas;
+
+import Despacho.Data.entidades.Receta;
+import javax.xml.bind.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorDatosRecetas {
+
+    private final File archivo = new File("listarecetas.xml");
+
+    public void guardar(List<Receta> lista) {
+        try {
+            Listas listas = new Listas();
+            lista.forEach(listas::agregarReceta);
+
+            JAXBContext ctx = JAXBContext.newInstance(Listas.class);
+            Marshaller m = ctx.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            m.marshal(listas, archivo);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Receta> cargar() {
+        if (!archivo.exists()) {
+            return new ArrayList<>();
+        }
+        try {
+            JAXBContext ctx = JAXBContext.newInstance(Listas.class);
+            Unmarshaller um = ctx.createUnmarshaller();
+            Listas listas = (Listas) um.unmarshal(archivo);
+            return listas.getRecetas();
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+}
