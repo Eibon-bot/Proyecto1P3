0b733a9527217975e5af2d3dee9b77d52421457c
Waloe <54786573+Eibon-bot@users.noreply.github.com>
2025-08-30 19:06:09 -0600

    Unifique las listas en una sola clase, y modifique los gestores para que estas funcionaran

---
+++ src/main/java/Despacho/Presentation/View/MedicoView/MedicoView.java
@@ -0,0 +1,9 @@
+package Despacho.Presentation.View.MedicoView;
+
+import javax.swing.*;
+
+public class MedicoView {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTabbedPane tabbedPane2;
+}
