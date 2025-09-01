bcc8282812a0f498e1fe1d044ebbc9a9117197e5
Tu Nombre <tuemail@example.com>
2025-09-01 13:59:47 -0600

    farma

---
+++ src/main/java/Despacho/Presentation/Farmaceutico/FarmaAdmin.java
@@ -0,0 +1,17 @@
+package Despacho.Presentation.Farmaceutico;
+
+import javax.swing.*;
+
+public class FarmaAdmin {
+    private JLabel idlabelFarma;
+    private JTextField textFieldIdFarma;
+    private JLabel nombreFarma;
+    private JTextField textFieldNomFarma;
+    private JButton guardarButtonFarma;
+    private JButton limpiarButtonFarma;
+    private JButton borrarButtonFarma;
+    private JTextField textFieldBusqFarma;
+    private JButton buscarButtonFarma;
+    private JButton reporteButtonFarma;
+    private JComboBox comboBoxFar;
+}
