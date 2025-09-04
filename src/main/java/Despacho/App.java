ec82875f211303b84337c50ebf9de146c745d848
Waloe <54786573+Eibon-bot@users.noreply.github.com>
2025-09-03 18:16:03 -0600

    Prueba en App

--- src/main/java/Despacho/App.java
+++ src/main/java/Despacho/App.java
@@ -1,9 +1,12 @@
 package Despacho;
 
+import Despacho.Presentation.Prescribir.Prescribir;
 import Despacho.Presentation.View.*;
 import Despacho.Presentation.Login.Controller;
 import Despacho.Presentation.Login.Model;
 import Despacho.Data.Registro.AuthService;
+import Despacho.Data.Listas.GestorDatosFarmaceuticos;
+import Despacho.Data.Listas.GestorDatosMedicos;
 
 import javax.swing.*;
 import java.awt.*;
@@ -17,9 +20,10 @@ public class App {
         }
 
         try {
+            GestorDatosFarmaceuticos gf = new GestorDatosFarmaceuticos();
+            GestorDatosMedicos gm = new GestorDatosMedicos();
             Model model = new Model();
-            UsuarioDAO uDAO = new UsuarioDAO("target/classes/usuarios.xml");
-            AuthService auth = new AuthService(uDAO);
+            AuthService auth = new AuthService(gm,gf);
             login login = new login();
             Controller controller = new Controller(login, model, auth);
 
@@ -29,6 +33,14 @@ public class App {
             window.setTitle("Login");
             window.setContentPane(login.getLogin());
             window.setVisible(true);
+//para probar
+            JFrame windowPrescribir = new JFrame();
+            windowPrescribir.setTitle("Preescribir");
+            windowPrescribir.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            windowPrescribir.setContentPane(new Prescribir().getPrescribir());
+            windowPrescribir.pack();
+            windowPrescribir.setLocationRelativeTo(null); // Centrar la ventana
+            windowPrescribir.setVisible(true);
 
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Error al iniciar la aplicación: " + e.getMessage());
