34021964ebf2333fc34913353d4f0b73e66c692a
Waloe <54786573+Eibon-bot@users.noreply.github.com>
2025-09-10 19:35:00 -0600

    Historico(revisar)

---
+++ src/main/java/Despacho/Presentation/Historico/Model.java
@@ -0,0 +1,46 @@
+package Despacho.Presentation.Historico;
+import Despacho.AbstractModel;
+import Despacho.Logic.Entidades.Receta;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.beans.PropertyChangeListener;
+
+public class Model extends AbstractModel {
+    Receta current;
+    List<Receta> list;
+
+    public static final String CURRENT = "current";
+    public static final String LIST = "list";
+
+    public Model() {
+        current = new Receta();
+        list = new ArrayList<>();
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        super.addPropertyChangeListener(listener);
+        firePropertyChange(CURRENT);
+        firePropertyChange(LIST);
+    }
+
+    public Receta getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(Receta current) {
+        this.current = current;
+        firePropertyChange(CURRENT);
+    }
+
+    public List<Receta> getList() {
+        return list;
+    }
+
+    public void setList(List<Receta> list) {
+        this.list = list;
+        firePropertyChange(LIST);
+    }
+}
+
